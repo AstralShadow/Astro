621040e3ac04fb3841a7bb0a7e33fa4fd05ca849
AstralShadow <47869198+AstralShadow@users.noreply.github.com>
2020-04-26 21:46:10 +0300

    Update initializeSession.php

--- include/initializeSession.php
+++ include/initializeSession.php
@@ -1,9 +1,18 @@
 <?php
 
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+/**
+ * @global mixed $_SESSION["user"]
+ * @global Timestamp $_SESSION["creationTimestamp"]
  */
 
 session_name("Astro_v0_1");
+session_start();
+
+if (!isset($_SESSION["user"])) {
+    $_SESSION["user"] = null;
+}
+
+if (!isset($_SESSION["creationTimestamp"])) {
+    $_SESSION["creationTimestamp"] = time();
+}
+
