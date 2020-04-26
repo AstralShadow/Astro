3506c8523999c307a11d277b3591b79a9e59049c
AstralShadow <47869198+AstralShadow@users.noreply.github.com>
2020-04-26 22:23:26 +0300

    Update index.php

--- index.php
+++ index.php
@@ -1,9 +1,21 @@
 <?php
-
-/* 
+/*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
 
 include "include/initializeSession.php";
+?>
+<!DOCTYPE HTML>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>
+            Astro
+        </title>
+    </head>
+    <body>
+        <div>
+    </body>
+</html>
